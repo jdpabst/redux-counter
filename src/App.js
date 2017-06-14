e3d747e0337818384a9f6a011f61b1c54ff2b0ef
Jessica <agro-chick@hotmail.com>
2017-06-14 13:15:18 -0600

    finished mini

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import { connect } from 'react-redux';
+import { increment, decrement} from './ducks/counter';
 import "./App.css";
 
 export class App extends Component {
@@ -7,29 +8,29 @@ export class App extends Component {
 		return (
 			<div className="app">
 				<section className="counter">
-					<h1 className="counter__current-value">{ 0 }</h1>
+					<h1 className="counter__current-value">{this.props.counter}</h1>
 					<div className="counter__button-wrapper">
 						<button
 							className="counter__button increment-one"
-							onClick={ () => null }
+							onClick={ () => this.props.increment(1) }
 						>
 							+1
 						</button>
 						<button
 							className="counter__button increment-five"
-							onClick={ () => null }
+							onClick={ () => this.props.increment(5) }
 						>
 							+5
 						</button>
 						<button
 							className="counter__button decrement-one"
-							onClick={ () => null }
+							onClick={ () => this.props.decrement(1) }
 						>
 							-1
 						</button>
 						<button
 							className="counter__button decrement-five"
-							onClick={ () => null }
+							onClick={ () => this.props.decrement(5) }
 						>
 							-5
 						</button>
@@ -60,4 +61,8 @@ export class App extends Component {
 	}
 }
 
-export default App;
+// Here you can pick and choose what you want from the state for the App.js, we want to whole state for this app.  TOP LEVEL COMPONENTS GET THESE, CHILDREN COMPONENTS GET PROPS///
+function mapStateToProps(state){
+	return Object.assign({}, state);
+}
+export default connect(mapStateToProps, { increment, decrement })(App);
--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,64 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
+
+var initialState = {
+    counter: 0,
+    futureValues: [],
+    previousValues: []
+}
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const REDO = "REDO";
+const UNDO = "UNDO";
+
+export function increment(amnt){
+    return {
+        type: INCREMENT,
+        payload: amnt
+    }
+}
+
+export function decrement(amnt){
+    return {
+        type: DECREMENT,
+        payload: amnt
+    }
+}
+export function redo(){
+    return {
+        type: REDO,
+    }
+}
+export function undo(){
+    return {
+        type: UNDO,
+    }
+}
+
+
+// 'state = initialState' -- if passed null or undefined, pass in initialState instead...///
+function counter(state = initialState, action){
+    state = Object.assign(state, { 
+        lastAction: action 
+    })
+    switch(action.type){
+       case INCREMENT:
+        return Object.assign({}, state, {
+            counter: state.counter + action.payload,
+            previousValues: [state.counter, ...state.previousValues]
+        })
+       case DECREMENT:
+        return Object.assign({}, state, {
+            counter: state.counter - action.payload,
+            previousValues: [state.counter, ...state.previousValues]
+        })
+       case REDO:
+        return Object.assign({}, state, {
+            
+        })
+    }
+}
+
+
+export default counter
